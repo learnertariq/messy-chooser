8f25a9df5b3429c335e2492ed8adfac7fb695a85
Tariqul Islam <usukuk@gmail.com>
2022-03-26 18:03:02 +0600

    second question answered

--- src/components/QnA.jsx
+++ src/components/QnA.jsx
@@ -14,6 +14,20 @@ const qnaData = [
       "In a React Project components are well organized",
     ],
   },
+  {
+    q: "Props vs States",
+    a: [
+      "Props are used to pass data from one component to another component and State data is passed inside the component only.",
+      "Props can be used to populate state and can be used with any kind of components. But at earlier days state was only used in a class component",
+      "You cannot the data of Props, but you can change sate data as many times as you want.",
+      "You can only read Props, but States can be both read and written",
+      "Props is passed as an attribute or key from an component to other, and State can be declared using useState hook in functional component",
+    ],
+  },
+  {
+    q: "How useState works",
+    a: [""],
+  },
 ];
 
 const QnA = () => {
@@ -21,30 +35,28 @@ const QnA = () => {
     <section className="container">
       <h2 className="text-center">FAQ for this Assignment :)</h2>
       <div className="qna-container row row-cols-1 row-cols-md-2 row-cols-lg-3 g-2">
-        <div className="col">
-          <article className="qna border border-success rounded shadow">
-            {qnaData.map((item) => (
-              <>
-                <div
-                  style={styles.qContainer}
-                  className="q-container d-flex align-items-center justify-content-between bg-light p-3"
-                >
-                  <h3 style={styles.qHeading} className="fs-4">
-                    {item.q}
-                  </h3>
-                  <i className="fa-solid fa-chevron-down fs-4"></i>
-                </div>
-                <div className="a-container p-3">
-                  <ul style={styles.answer}>
-                    {item.a.map((item) => (
-                      <li>{item}</li>
-                    ))}
-                  </ul>
-                </div>
-              </>
-            ))}
-          </article>
-        </div>
+        {qnaData.map((item) => (
+          <div className="col">
+            <article className="qna border border-success rounded shadow">
+              <div
+                style={styles.qContainer}
+                className="q-container d-flex align-items-center justify-content-between bg-light p-3"
+              >
+                <h3 style={styles.qHeading} className="fs-4">
+                  {item.q}
+                </h3>
+                <i className="fa-solid fa-chevron-down fs-4"></i>
+              </div>
+              <div className="a-container p-3">
+                <ul style={styles.answer}>
+                  {item.a.map((item) => (
+                    <li>{item}</li>
+                  ))}
+                </ul>
+              </div>
+            </article>
+          </div>
+        ))}
       </div>
     </section>
   );
